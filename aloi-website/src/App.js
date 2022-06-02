417601385652c012cbf50d774980ef40161581ed
gabealoi <gabealoi@iu.edu>
2022-06-01 22:19:30 -0400

    card implementation

--- aloi-website/src/App.js
+++ aloi-website/src/App.js
@@ -1,17 +1,26 @@
+import { lazy, Suspense } from 'react';
 import {Routes, Route} from 'react-router-dom'
-import { Home } from './components/home';
+// import { Home } from './routes/home';
 import { Navbar } from './components/Navbar';
-import { Profile } from './components/profile';
+// import { Profile } from './routes/profile';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Home = lazy(() => import('./routes/home'));
+const Profile = lazy(() => import('./routes/profile'));
+
 
 function App() {
-  return (
 
+  return (
   <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='profile' element={<Profile />}></Route>
-    </Routes>
+      <Navbar />
+
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='profile' element={<Profile />}></Route>
+        </Routes>
+      </Suspense>
   </>
 
   );
