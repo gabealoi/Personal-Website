924575edbcd899e5428a33f80f91b744fbb24e24
gabealoi <gabealoi@iu.edu>
2022-06-03 11:44:49 -0400

    slideshow fully implemented

--- aloi-website/src/components/slider.js
+++ aloi-website/src/components/slider.js
@@ -1,6 +1,10 @@
 import AwesomeSlider from 'react-awesome-slider';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
+import arms_wide from '../images/arms_wide.jpg';
+import climbing from '../images/climbing.JPG';
+import umbrella from '../images/umbrellas.jpg';
+import mountains from '../images/moutains.jpg';
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
@@ -17,10 +21,10 @@ const Slider = () => {
     >
       
 
-      <div data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
-      <div data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
-      <div data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
-      <div data-src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+      <div data-src={arms_wide}/>
+      <div data-src={climbing} />
+      <div data-src={umbrella} />
+      <div data-src={mountains} />
     </AutoplaySlider>
   )
 }
--- aloi-website/src/routes/home.js
+++ aloi-website/src/routes/home.js
@@ -12,17 +12,21 @@ const Home = () => {
         <div class="home-page">
 
             <div class="home-body">
-                <div class="slide">
-                    <Slider></Slider>
-                </div>
 
                 <h1 class="home-header">Welcome to Me!</h1>
 
-                <article class="short-bio">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim sed faucibus turpis in eu mi bibendum neque egestas. Phasellus faucibus scelerisque eleifend donec pretium. Integer quis auctor elit sed vulputate mi. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Metus dictum at tempor commodo. Ut lectus arcu bibendum at varius vel. Arcu odio ut sem nulla pharetra diam sit amet. Sed egestas egestas fringilla phasellus. Convallis posuere morbi leo urna molestie at elementum. A arcu cursus vitae congue mauris rhoncus aenean vel. Luctus venenatis lectus magna fringilla urna. Curabitur vitae nunc sed velit. Proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper.
+                <div class="bio-slide-container">
+                    <article class="short-bio" id="item1">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim sed faucibus turpis in eu mi bibendum neque egestas. Phasellus faucibus scelerisque eleifend donec pretium. Integer quis auctor elit sed vulputate mi. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Metus dictum at tempor commodo. Ut lectus arcu bibendum at varius vel. Arcu odio ut sem nulla pharetra diam sit amet. Sed egestas egestas fringilla phasellus. Convallis posuere morbi leo urna molestie at elementum. A arcu cursus vitae congue mauris rhoncus aenean vel. Luctus venenatis lectus magna fringilla urna. Curabitur vitae nunc sed velit. Proin libero nunc consequat interdum varius sit. In fermentum posuere urna nec tincidunt praesent semper.
+                        Leo a diam sollicitudin tempor id. Dapibus ultrices in iaculis nunc sed augue. Dui nunc mattis enim ut. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Aliquam faucibus purus in massa tempor nec feugiat nisl. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Purus in mollis nunc sed id semper risus. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. Enim nulla aliquet porttitor lacus luctus accumsan tortor.
+                    </article>
 
-                    Leo a diam sollicitudin tempor id. Dapibus ultrices in iaculis nunc sed augue. Dui nunc mattis enim ut. Lobortis feugiat vivamus at augue eget arcu dictum varius duis. Aliquam faucibus purus in massa tempor nec feugiat nisl. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero nunc. Purus in mollis nunc sed id semper risus. Placerat in egestas erat imperdiet sed euismod nisi porta lorem. Auctor augue mauris augue neque gravida in fermentum et sollicitudin. Enim nulla aliquet porttitor lacus luctus accumsan tortor.
-                </article>
+                    <div class="slide" id="item2">
+                        <a href="./profile">
+                            <Slider></Slider>
+                        </a>
+                    </div>
+                </div>
 
                 <div class="banner">
 
